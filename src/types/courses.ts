bbafd28c26c6cdb2da744f5aaa8d630d36d427ae
DTNgoc-II0A <dangngoc4332@gmail.com>
2025-09-26 10:22:41 +0700

    Update courses.ts

--- src/types/courses.ts
+++ src/types/courses.ts
@@ -9,6 +9,19 @@ export type Category = {
     createdDate: string
 }
 
+export interface Lesson {
+  id: string; // uuid
+  courseId: string; // uuid
+  title: string; // varchar(255)
+  contentUrl?: string; // varchar(255) - nullable
+  requireRobot: boolean; // bool
+  duration: number; // int4 (in seconds)
+  content?: string; // varchar(255) - nullable
+  contentType: string; // varchar(255)
+  orderNumber: number; // int4
+  solution?: any; // jsonb - nullable, can be more specific based on your JSON structure
+}
+
 export interface Course {
   id: string; // uuid
   name: string; // varchar(255)
@@ -24,4 +37,5 @@ export interface Course {
   categoryId: string; // uuid
   slug: string; // varchar(255)
   imageUrl?: string; // varchar(255) - assuming nullable
+  lessons?: Lesson[]
 }
