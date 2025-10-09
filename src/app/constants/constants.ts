044831734ae027c2808d54e935139815fdf62083
mineduck1608 <ducdm160804@gmail.com>
2025-10-09 10:35:34 +0700

    Update constants.ts

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -5,6 +5,6 @@ export const apiCoursesUrl = "https://course-service.alpha-code.site/api/v1"
 export const apiUsersUrl = "https://user-service.alpha-code.site/api/v1"
 export const apiActivitiesUrl = "https://activity-service.alpha-code.site/api/v1"
 export const apiRobotsUrl = "https://robot-service.alpha-code.site/api/v1"
-//export const socketUrl = "wss://backend.alpha-code.site/ws";
-export const socketUrl = "ws://192.168.1.140:8000/ws";
+export const socketUrl = "wss://backend.alpha-code.site/ws";
+//export const socketUrl = "ws://192.168.1.140:8000/ws";
 
