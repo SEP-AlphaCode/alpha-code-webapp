c6db76a52f0d91f0ede6b1bf4f3e38819a57ca35
mineduck1608 <ducdm160804@gmail.com>
2025-10-25 12:27:07 +0700

    update status

--- src/app/payment/result/page.tsx
+++ src/app/payment/result/page.tsx
@@ -1,9 +1,18 @@
+import LoadingState from "@/components/loading-state";
 import PaymentResultPage from "@/components/payment/payment-result-page";
 import { Suspense } from "react";
 
 export default function Page() {
   return (
-    <Suspense fallback={null}>
+    <Suspense
+      fallback={
+        <div
+          className="min-h-screen flex items-center justify-center"
+        >
+          <LoadingState />
+        </div>
+      }
+    >
       <PaymentResultPage />
     </Suspense>
   );
