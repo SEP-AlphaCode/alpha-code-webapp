f9dfc78783b07727eaf3217da445d8aa1f48943c
mineduck1608 <ducdm160804@gmail.com>
2025-10-27 16:41:24 +0700

    Update license-key-api.ts

--- src/features/license-key/api/license-key-api.ts
+++ src/features/license-key/api/license-key-api.ts
@@ -2,7 +2,7 @@ import { paymentsHttp } from "@/utils/http";
 
 export const getLicenseKey = async (accountId: string): Promise<string> => {
     try {
-        const response = await paymentsHttp.get<string>('/license-keys/by-account/{accountId}/' + accountId)
+        const response = await paymentsHttp.get<string>('/license-keys/by-account/' + accountId)
         return response.data
     }
     catch (error) {
